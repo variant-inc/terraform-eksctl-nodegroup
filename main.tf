3a550378b94e73ca71426e9bca49b67c55270e56
Naveen <naveenp@drivevariant.com>
2022-01-06 17:52:40 -0500

    init changes

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "eksctl_cluster" "primary" {
+  eksctl_version = var.eksctl_version
+  name           = var.name
+  region         = var.region
+  version        = "1.18"
+  spec           = <<-EOS
+    iam:
+      withOIDC: true
+      serviceAccounts: [] # managed by terraform
+
+    managedNodeGroups:
+      - name: ${var.node_group_name}
+        instanceType: ${var.instance_type}
+        desiredCapacity: ${var.nodes_count}
+  EOS
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ versions.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  required_providers {
+    eksctl = {
+      source  = "mumoshu/eksctl"
+      version = "0.13.4"
+    }
+  }
+}
